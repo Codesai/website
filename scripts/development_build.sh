36b7c16c51a671c4de0f5cad04707658099116a6
Toño de la Torre <antonio@codesai.com>
2026-09-01 13:51:16 +0200

    fix: include posts in local English site

--- scripts/development_build.sh
+++ scripts/development_build.sh
@@ -2,4 +2,7 @@
 
 set -euo pipefail
 
+mkdir -p _i18n/en/_posts
+cp -a _i18n/es/_posts/. _i18n/en/_posts/
+
 bundle exec jekyll serve --host 0.0.0.0 --force_polling --config _config.yml,_local_config.yml
